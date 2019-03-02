7bd3b05e086101daf7b9768acb264b82f5d94246
Todd Schavey <todd.schavey@ge.com>
2019-03-02 09:19:33 -0500

    updated client-app AppModule for Material Animations

--- projects/client-app/src/app/app.module.ts
+++ projects/client-app/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Angular Material imports
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
