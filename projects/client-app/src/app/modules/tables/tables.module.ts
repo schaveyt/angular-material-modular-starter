57d75d1f25ae19b9f669ad762fb4931d9ddcb53b
Todd Schavey <todd.schavey@ge.com>
2019-03-11 22:47:18 -0400

    client-app/tables: registered MaterialModule to the TablesModule

--- projects/client-app/src/app/modules/tables/tables.module.ts
+++ projects/client-app/src/app/modules/tables/tables.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasicTableComponent } from './basic-table/basic-table.component';
 import { TablesRoutingModule } from './tables-routing/tables-routing.module';
+import { MaterialModule } from './../material/material.module'
 
 @NgModule({
   declarations: [BasicTableComponent],
   imports: [
     CommonModule,
+    MaterialModule,
     TablesRoutingModule
   ]
 })
